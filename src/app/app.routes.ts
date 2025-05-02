f0a6888019795b54b20c7d7e545999832de39861
NaN.ssel <romero.juan.ignacio.1294@gmail.com>
2025-05-02 11:23:01 -0300

    modified app component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './views/layout/layout.component';
+import { HomeComponent } from './views/pages/home/home.component';
+import { LoginComponent } from './views/pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: '', redirectTo: 'home', pathMatch: 'full' }
+    ]
+  },
+  { path: '**', redirectTo: 'login' }
+];
